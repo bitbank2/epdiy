524837b1fbc5c8bc6116abd146ce8346c1b00bf3
Laurence Bank <laurencebank@Laurences-Air.home>
2024-02-10 13:55:24 +0000

    Initial support for Kaleido color EPD

--- examples/color/main/main.c
+++ examples/color/main/main.c
@@ -7,11 +7,90 @@
 #include "esp_sleep.h"
 #include <epdiy.h>
 #include "epd_highlevel.h"
+#include "JPEGDEC.h"
+//#include "bart_400x168.h" // 400x346 test image
+//#include "epd_color_test.h"
+#include "iron.h"
+
+JPEGIMAGE jpg; // static JPEG decoder structure
 
 EpdiyHighlevelState hl;
-int temperature = 25;
+int temperature = 20;
 // Buffers
 uint8_t *fb;            // EPD 2bpp buffer
+
+//
+// Treat the display as if each pixel is part of a Bayer pattern
+//
+void fine_pixel(int x, int y, uint16_t u16Color, uint8_t *fb)
+{
+int i = (x+y) % 3; // which color filter is in use?
+uint8_t u8, u8C;
+
+        if (x > 1447 || y > 1071) return;
+        if (i == 0) {
+           u8C = (u16Color >> 7) & 0xf; // green
+        } else if (i == 1) {
+           u8C = (u16Color >> 1) & 0xf; // blue
+        } else {
+           u8C = (u16Color >> 12);      // red
+        }
+	fb += (y * (1448/2)) + (x/2); // point to correct byte
+        u8 = fb[0];
+        if (x & 1) { // right pixel in MSN
+           u8 &= 0xf; u8 |= (u8C << 4);
+        } else {
+	   u8 &= 0xf0; u8 |= u8C;
+        }
+	fb[0] = u8;
+} /* fine_pixel() */
+
+//
+// Treat the display as if each pixel is a 3x3 block of pixels
+// The color is passed as RGB565 and displayed as RGB444
+//
+void chunky_pixel(int x, int y, uint16_t u16Color, uint8_t *fb)
+{
+int tx, ty;
+uint8_t u8, u8C, *d;
+int iFilter;
+uint8_t u8RGB[4];
+
+   if (x > 482 || y > 357) return;
+	// Convert RGB565 into 4-bits of G/B/R
+	u8RGB[0] = (u16Color >> 7) & 0xf; // green
+	u8RGB[1] = (u16Color >> 1) & 0xf; // blue
+	u8RGB[2] = (u16Color >> 12);      // red
+	x = x*3; y = y*3; // 3x3 pixel block
+	for (ty=y; ty<y+3; ty++) {
+	        d = &fb[(ty * (1448/2)) + (x/2)];
+		iFilter = (x+ty) % 3; 
+		for (tx=x; tx<x+3; tx++) {
+			u8 = d[0];
+			u8C = u8RGB[iFilter];
+		        if (tx & 1) { // right pixel in MSN
+           			u8 &= 0xf; u8 |= (u8C << 4);
+				*d++ = u8;
+        		} else {
+           			u8 &= 0xf0; u8 |= u8C;
+				*d = u8;
+        		}
+			iFilter++;
+			if (iFilter == 3) iFilter = 0;
+		} // for tx
+	} // for ty
+} /* chunky_pixel() */
+
+int JPEGDraw(JPEGDRAW *pDraw)
+{
+   uint16_t *s = pDraw->pPixels;
+      for (int cy=0; cy < pDraw->iHeight; cy++) {
+         for (int cx=0; cx<pDraw->iWidth; cx++) {
+            chunky_pixel(pDraw->x+cx,pDraw->y+cy,*s++, hl.front_fb);
+         }
+      }
+   return 1;
+} /* JPEGDraw() */
 
 int color_test() {
   EpdRect epd_area = {
@@ -21,28 +100,11 @@ int color_test() {
       .height = epd_height()
   };
 
-  uint8_t color_of = 255;
-  /**
-   * @brief PATTERN
-   * pix1     2    3  X
-   *    B     G    R  row:1
-   *    G     R    B  row:2
-   *    R     B    G  row:3
-   */
-  for (uint16_t y=1; y<601; y++) {
-    for (uint16_t x=1; x<epd_area.width; x++) {
-      // x, y, r, g, b
-      if (y < 200) {
-        epd_draw_cpixel(x, y, x/6, color_of, color_of, hl.front_fb);
-      } else if (y >= 200 && y < 400) {
-        epd_draw_cpixel(x, y, color_of, x/6, color_of, hl.front_fb);
-      } else {
-        epd_draw_cpixel(x, y, color_of, color_of, x/6, hl.front_fb);
-      }
-    }
-    //vTaskDelay(1);
-  }
-  
+  JPEG_openRAM(&jpg, iron, sizeof(iron), JPEGDraw);
+//  JPEG_openRAM(&jpg, bart_400x168, sizeof(bart_400x168), JPEGDraw);
+  jpg.ucPixelType = RGB565_LITTLE_ENDIAN;
+  JPEG_decode(&jpg, 0, 0, 0);
+  JPEG_close(&jpg);
 
   enum EpdDrawError _err = epd_hl_update_screen(&hl, MODE_GC16, temperature);
   epd_poweroff();
@@ -77,11 +139,14 @@ void app_main() {
 
 
   color_test();
-  printf("color example\n");
+ // printf("color example\n");
 
-  vTaskDelay(pdMS_TO_TICKS(5000));
-  esp_sleep_enable_timer_wakeup(DEEP_SLEEP_SECONDS * USEC);
-  esp_deep_sleep_start();
+  while (1) {
+     vTaskDelay(100);
+  }
+//  vTaskDelay(pdMS_TO_TICKS(5000));
+//  esp_sleep_enable_timer_wakeup(DEEP_SLEEP_SECONDS * USEC);
+//  esp_deep_sleep_start();
 }
 
 #endif
--- examples/dragon/main/main.c
+++ examples/dragon/main/main.c
@@ -34,7 +34,8 @@ void idf_loop() {
 }
 
 void idf_setup() {
-    epd_init(&DEMO_BOARD, &ED097TC2, EPD_LUT_64K);
+//    epd_init(&DEMO_BOARD, &ED097TC2, EPD_LUT_64K);
+    epd_init(&DEMO_BOARD, &ED060XC3, EPD_LUT_64K);
     epd_set_vcom(1560);
     hl = epd_hl_init(EPD_BUILTIN_WAVEFORM);
 }
